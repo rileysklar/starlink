a2521e61c78b04344b89278cde38d862330f8200
Riley Sklar <riley.sklar@gmail.com>
2024-05-03 16:42:25 -0500

    next2

--- src/components/BookingCalendar.jsx
+++ src/components/BookingCalendar.jsx
@@ -201,8 +201,16 @@ export default function BookingCalendar() {
               darkMode ? "" : "light-mode"
             } flex flex-row justify-center items-center text-right gap-2 px-4`}
           >
-            Next <img  fill: var(--text-color);
- src="./arrow-r.svg" alt="Next" />
+            Next
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M10,20A10,10,0,1,0,0,10,10,10,0,0,0,10,20ZM8.711,4.3l5.7,5.766L8.7,15.711,7.3,14.289l4.289-4.242L7.289,5.7Z" />
+            </svg>
           </button>
         </div>
       </div>
