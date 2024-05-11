ce99df9865e730a96452ff8f69e43311c2105ee8
Riley Sklar <riley.sklar@gmail.com>
2024-05-11 16:46:07 -0500

    darkmode: false

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./TopNav.css";
 
 export default function TopNav() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
     document.body.classList.toggle("light-mode", !darkMode);
@@ -14,19 +14,26 @@ export default function TopNav() {
         backgroundColor: "var(--background)",
         color: "var(--text-color)",
       }}
-      className="flex top-nav w-full h-auto p-4 justify-between"
+      className="flex top-nav w-full h-auto py-4 justify-between"
     >
-      <h1 className="text-2xl p-1 font-bold text-center">
-        📡 StarLink Rentals
-      </h1>
-      <button
+      <a href="/">
+        <h1 className="text-2xl p-1 font-bold text-center">
+          📡 StarLink Rentals
+        </h1>
+      </a>
+      {/* <button
         className={`px-4 py-2 rounded-full button-element ${
           darkMode ? "" : "light-mode"
         }`}
         onClick={() => setDarkMode(!darkMode)}
       >
         {darkMode ? "Light" : "Dark"} mode
-      </button>
+      </button> */}{" "}
+      <a href="#contact" className="">
+        <button className="px-4 py-2 rounded-full button-element">
+          Contact us
+        </button>
+      </a>
     </div>
   );
 }
